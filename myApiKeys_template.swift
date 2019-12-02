1c5a125621dfcf4077be8440fc3d4f16650328e2
bplatte <loipen-lady@web.de>
2019-12-02 22:22:04 +0100

    Application runs good

---
+++ myApiKeys_template.swift
@@ -0,0 +1,14 @@
+//
+//  myApiKeys.swift
+//  ARTranslate
+//
+//  Created by Benny Platte on 02.12.19.
+//  Copyright © 2019 hsmw. All rights reserved.
+//
+
+import Foundation
+
+struct myApiKeys {
+    static let GoogleTranslateApiKey:String = "InsertYourApiStringHere"
+}
+
